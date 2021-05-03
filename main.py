5103638c9189450c2c27b176920a50acc05af4d6
Amit Gadia <58513172+amit-gadia@users.noreply.github.com>
2021-05-03 23:29:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-# app.py 
-from flask import Flask
-app = Flask(__name__)
+from flask import *
+import requests
+import json
+app=Flask('__name__')
+aa=""
+@app.route('/',methods=['GET'])
+def funci():
+    aa=request.args.get('c')
+    print(aa)
+    if(aa!=""):
+        c='http://channelget.herokuapp.com/?c='+aa
+        s=requests.get(c).text
+        print(s)
+        dictt={}
+        dictt['live']=s
+        print(dictt)
+        return render_template("abc.html",ss=s,dic=dictt)
+    else:
+        c='http://channelget.herokuapp.com/?c='
+        s=requests.get(c).text
+        print(s)
+        return(s)
+app.run(port=5000)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-if __name__ == "__main__":
-    app.run(port=80)
+
+
